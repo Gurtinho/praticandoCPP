35675f2ad17a01aa264f845c1762554be47603d4
Gustavo Litter <gustavolitter@gmail.com>
2025-03-13 23:57:38 -0300

    Adicionar arquivos de projeto.

---
+++ praticandoCPP.cpp
@@ -0,0 +1,41 @@
+﻿// praticandoCPP.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "praticandoCPP.h"
+
+using namespace std;
+
+#define PI 3.14;
+
+int main()
+{
+	cout << PI;
+	cout << "\n";
+
+
+	// enums
+	enum armas { fuzil = 100, carabina = 200, shotgun = 300 };
+	armas armaSelecionada;
+	armaSelecionada = fuzil; // 100
+	cout << armaSelecionada << "\n";
+
+
+
+	// pilha - stack
+	stack <string> cartas;
+	cartas.push("rei");
+	cartas.push("rainha");
+	cartas.push("valete"); // vai sair
+	cout << "tamanho da pilha: " << cartas.size() << "\n";
+	cartas.pop();
+	cout << cartas.top() << "\n";
+
+
+
+	// filas - queue
+
+
+
+
+	return 0;
+}
---
+++ praticandoCPP.h
@@ -0,0 +1,11 @@
+﻿// praticandoCPP.h: arquivo de inclusão para arquivos de inclusão padrão do sistema,
+// ou arquivos de inclusão específicos a um projeto.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include <stack> // Biblioteca pilhas
+#include <queue> // Biblioteca filas
+
